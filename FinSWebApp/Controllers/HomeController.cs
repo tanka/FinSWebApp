ebd78a69d9553813cdc58b4fb69cb72d86ac1da5
tanka <sharma.tankanath@gmail.com>
2024-12-06 22:44:38 -0600

    New Index Page

--- FinSWebApp/Controllers/HomeController.cs
+++ FinSWebApp/Controllers/HomeController.cs
@@ -1,20 +1,29 @@
+using FinSWebApp.Areas.Identity.Data;
 using FinSWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace FinSWebApp.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
         {
+            //ViewData["UserId"] = _userManager.GetUserId(this.User);
+            //ViewData["UserName"] = _userManager.GetUserName(this.User);
+            //ViewData["Title"] = "Home Page";
             return View();
         }
 
